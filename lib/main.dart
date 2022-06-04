0f43b324f97341aa7ae65f7b27ef5acd1d60416d
AquaApple <yassershehab9@gmail.com>
2022-06-05 00:07:27 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyHomePage());
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+      ),
+      body: Center(child: Text('Widget Playground!')),
+    );
+  }
+}
