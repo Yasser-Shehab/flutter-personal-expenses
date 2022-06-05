7ec276bc7cd411b5ac9a6c78ba4bbf33513ddbed
Yasser-Shehab <yassershehab9@gmail.com>
2022-06-05 10:45:16 +0200

    Added Column and Widget Children With Container styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,43 @@
+// ignore_for_file: prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors
+// ignore_for_file: sized_box_for_whitespace
+// ignore_for_file: sort_child_properties_last
+
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyHomePage());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter App'),
+      appBar: AppBar(title: Text('Flutter App')),
+      body: Column(
+        children: <Widget>[
+          Card(
+            margin: EdgeInsets.all(20),
+            color: Colors.blue,
+            child: Container(
+              width: double.infinity,
+              child: Text('CHART!!'),
+            ),
+            elevation: 5,
+          ),
+          Card(
+            child: Text('List of TX'),
+          ),
+        ],
       ),
-      body: Center(child: Text('Widget Playground!')),
     );
   }
 }
